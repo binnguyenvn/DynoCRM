45e4b3a17b22cb8645fd3a0e5be1a77589a5ffe5
Bin <bin.ngoctu@gmail.com>
2020-01-06 16:03:52 +0700

    Default field for Contact

--- risocrm/app_mgmt/migrations/0002_initial_db.py
+++ risocrm/app_mgmt/migrations/0002_initial_db.py
@@ -2,10 +2,90 @@ from django.conf import settings
 from django.db import migrations, models
 
 from risocrm.app_mgmt.models import Dynafield
+from risocrm.choices.models import Choice
 
 
 def update_field_forward(apps, schema_editor):
-    pass
+    city = Choice.objects.get(name="City")
+    rate = Choice.objects.get(name="Rate")
+    source = Choice.objects.get(name="Source")
+    Dynafield.objects.create(
+        module="Contact",
+        name="phone",
+        type="CharField",
+        verbose_name="Phone number",
+        fkmodule="",
+        on_delete="",
+        default="",
+        max_length=20,
+        group="Contact Infomation",
+        option=None)
+    Dynafield.objects.create(
+        module="Contact",
+        name="address",
+        type="CharField",
+        verbose_name="Address",
+        fkmodule="",
+        on_delete="",
+        default="",
+        max_length=1000,
+        group="Contact Infomation",
+        option=None)
+    Dynafield.objects.create(
+        module="Contact",
+        name="city",
+        type="ForeignKey",
+        verbose_name="City",
+        fkmodule="Choice",
+        on_delete="DO_NOTHING",
+        default="",
+        max_length=None,
+        group="Contact Infomation",
+        option=city)
+    Dynafield.objects.create(
+        module="Contact",
+        name="rate",
+        type="ForeignKey",
+        verbose_name="Rate",
+        fkmodule="Choice",
+        on_delete="DO_NOTHING",
+        default="",
+        max_length=None,
+        group="Contact Infomation",
+        option=rate)
+    Dynafield.objects.create(
+        module="Contact",
+        name="source",
+        type="ForeignKey",
+        verbose_name="Source",
+        fkmodule="Choice",
+        on_delete="DO_NOTHING",
+        default="",
+        max_length=None,
+        group="Contact Infomation",
+        option=source)
+    Dynafield.objects.create(
+        module="Contact",
+        name="facebook",
+        type="CharField",
+        verbose_name="Facebook name",
+        fkmodule="",
+        on_delete="",
+        default="",
+        max_length=100,
+        group="Facebook Infomation",
+        option=None)
+    Dynafield.objects.create(
+        module="Contact",
+        name="facebook_url",
+        type="CharField",
+        verbose_name="Facebook url",
+        fkmodule="",
+        on_delete="",
+        default="",
+        max_length=200,
+        group="Facebook Infomation",
+        option=None)
 
 
 def update_field_backward(apps, schema_editor):
@@ -14,6 +94,6 @@ def update_field_backward(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [("app_mgmt", "0001_initial")]
+    dependencies = [("choices", "0002_initial_db"), ("app_mgmt", "0001_initial")]
 
     operations = [migrations.RunPython(update_field_forward, update_field_backward)]
---
+++ risocrm/choices/migrations/0002_initial_db.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.db import migrations, models
+
+from risocrm.choices.models import Choice
+
+
+def update_user_forward(apps, schema_editor):
+    """Set site domain and name."""
+    Choice.objects.create(name="City")
+    Choice.objects.create(name="Rate")
+    Choice.objects.create(name="Source")
+
+
+def update_user_backward(apps, schema_editor):
+    """Revert site domain and name to default."""
+    Choice.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("choices", "0001_initial")]
+
+    operations = [migrations.RunPython(update_user_forward, update_user_backward)]
